a44c4aaa3ff5a4a92e4acb9cdb9fba32581cae42
Akifrm <61554024+Akifrm@users.noreply.github.com>
2024-04-22 17:46:44 +0300

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import { connect } from 'mongoose';
+import users from './model/users.js';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.port || 3000;
+const url = "mongodb+srv://akifcagliworkforweb:saergkweprgwergwerg5864we5rg41w6erg@cluster0.h75cbbf.mongodb.net/Cluster1?retryWrites=true&w=majority";
+
+connect(url).then(() => {
+    console.log("MongoDB'ye başarıyla bağlanıldı");
+})
+
+app.use(cors({
+    origin: 'http://127.0.0.1:5500/',
+    credentials: true,
+    optionSuccessStatus: 200
+}));
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.post('/:req', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const { params } = req;
+
+    console.log(req.headers['x-forwarded-for'], req.socket.remoteAddress, req.ip, params, req.connection.remoteAddress);
+
+    users.updateOne({ ip }, { [params.req]: params.data }, { upsert: true }).then(() => {
+        res.status(200).json({ status: 'ok' });
+    }).catch(err => {
+        res.status(500).json({ status: 'error' });
+        console.log(err);
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Sunucu ${port} numaralı port üzerinde çalışıyor`);
+});
